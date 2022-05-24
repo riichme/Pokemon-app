de764b6b0262d10b313d92ca1095fe385b071666
ristmi <ristmidaffahoiri@gmail.com>
2022-05-24 15:33:09 +0700

    ubahpokemon

--- src/App.js
+++ src/App.js
@@ -1,60 +1,45 @@
-import React from "react";
-import './App.css';
+import "./App.css";
 
-const inputKeyword = document.querySelector('.input-keyword');
-class App extends React.Component {
-  
-	// Constructor
-	constructor(props) {
-		super(props);
+import { useState } from "react";
 
-		this.state = {
-			items: [],
-			DataisLoaded: false
-		};
-	}
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [inputId, setInputId] = useState("");
 
-	// ComponentDidMount is used to
-	// execute the code
-	componentDidMount() {
-		fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputKeyword.value +"&appid=1fe5f03e8b679377cbc41601289edfdd&units=metc")
-			.then((res) => res.json())
-      .then(response => {
-        let result = document.querySelector('.result')
-        result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
-                            <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
-                            <p style="margin-bottom: 17px;">Temperature from ${response.main.temp_min}°С to ${response.main.temp_max}°С</p>
-                            <h5>Wind Speed : ${response.wind.speed} m/s</h5>
-                            <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
-                            <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>` 
-      })
-			.then((json) => {
-				this.setState({
-					items: json,
-					DataisLoaded: true
-				});
-			})
-      inputKeyword.value = null;
-	}
-	render() {
-		const { DataisLoaded, items } = this.state;
-		if (!DataisLoaded) return <div>
-			<h1> Pleses wait some time.... </h1> </div> ;
+  function getData() {
+    fetch(`https://api.pokemontcg.io/v1/cards?q=name:${inputId}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }
 
-		return (
-		<div className = "App">
-			<h1> Fetch data from an api in react </h1> {
-				items.map((item) => (
-				<ol key = { item } >
-					User_Name: { item },
-					Full_Name: { item },
-					User_Email: { item }
-					</ol>
-				))
-			}
-		</div>
-	);
+  function resetData() {
+    setItems([]);
+    setInputId("");
+  }
+
+  // useEffect(() => {
+  //   fetch(`https://api.pokemontcg.io/v1/cards`)
+  //     .then(response => response.json())
+  //     .then(json => setItems(json))
+  // }, [])
+  let arr = []
+  arr.push(items)
+  console.log(items.cards, "<<< ARR")
+  return (
+    <div className="App">      
+      <input value={inputId} onChange={(e) => setInputId(e.target.value)} />
+      <button onClick={() => getData()}>cari</button>
+      <button onClick={() => resetData()}>reset</button>
+
+      <div>
+        { items.cards ? items.cards.map(itm => (
+          <div>
+           <img style={{width: '20%', height: '20%', marginLeft: '50px', marginBottom: '20px', marginTop: '20px', position: 'center'}} key={itm.id} src={itm.imageUrl} alt={itm.name}></img>
+            
+            <br />
+          </div>
+        )): ''}
+      </div>
+    </div>
+  );
 }
-}
-
-export default App;
