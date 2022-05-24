8c1d3ce4c7b1d6912aa6abe39060956e2a41f494
hidayahhtaaufik <taufiq.hidayah@koinworks.com>
2022-05-24 15:18:11 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { useState } from "react";
 import "./App.css";
+
+import { useState } from "react";
 
 export default function App() {
   const [items, setItems] = useState([]);
   const [inputId, setInputId] = useState("");
 
   function getData() {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${inputId}&appid=1fe5f03e8b679377cbc41601289edfdd&units=metc`
-    )
-      .then((response) => response.json())
-      .then((json) => setItems(json));
+    fetch(`https://api.pokemontcg.io/v1/cards?q=name:${inputId}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
   }
 
   function resetData() {
@@ -19,33 +17,28 @@ export default function App() {
     setInputId("");
   }
 
+  // useEffect(() => {
+  //   fetch(`https://api.pokemontcg.io/v1/cards`)
+  //     .then(response => response.json())
+  //     .then(json => setItems(json))
+  // }, [])
+  let arr = []
+  arr.push(items)
+  console.log(items.cards, "<<< ARR")
   return (
-    <div className="App">
+    <div className="App">      
       <input value={inputId} onChange={(e) => setInputId(e.target.value)} />
       <button onClick={() => getData()}>cari</button>
       <button onClick={() => resetData()}>reset</button>
 
       <div>
-        {items.map((item) => (
+        { items.cards ? items.cards.map(itm => (
           <div>
-            <h2>
-              {item.name}, {item.sys.country}
-            </h2>
-            <h5>
-              <span class="temp">{item.main.temp}°С</span>{" "}
-              <span class="temp">{item.weather[0].description}</span>
-            </h5>
-            <p>
-              Temperature from {item.main.temp_min}°С to {item.main.temp_max}°С
-            </p>
-            <h5>Wind Speed : {item.wind.speed} m/s</h5>
-            <h5>Clouds : {item.clouds.all}%</h5>
-            <h4>
-              Geo Coordinates : [{item.coord.lat}, {item.coord.lon}]
-            </h4>
+           <img style={{width: '20%', height: '20%', marginLeft: '50px', marginBottom: '20px', marginTop: '20px', position: 'center'}} key={itm.id} src={itm.imageUrl} alt={itm.name}></img>
+            
             <br />
           </div>
-        ))}
+        )): ''}
       </div>
     </div>
   );
